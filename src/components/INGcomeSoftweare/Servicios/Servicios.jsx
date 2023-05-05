6903f309e4511be7a193e770bd21db0618aa69c6
augusto <augustoantonelli88@gmail.com>
2023-05-05 20:50:26 -0300

    re-work of servicios

--- src/components/INGcomeSoftweare/Servicios/Servicios.jsx
+++ src/components/INGcomeSoftweare/Servicios/Servicios.jsx
@@ -13,57 +13,74 @@ export default function Servicios() {
     return (
         <section id="servicios">
             <h1> ¿como podemos ayudarte? </h1>
-            <div className='servicios_container'>
+            <div className="servicios_card_container">
+
                 <motion.div
-                    initial={{ x: '-100%' }}
+                    className="servicios_card"
+                    initial={{ x: '500%' }}
                     whileInView={{ x: '0%' }}
                     transition={{ duration: 0.5 }}
                 >
 
-                    <div className='servicios_card diseño'>
-                        <div className='icon'>
-                            <Pencil />
-                        </div>
-                        <div className='text'>
-                            <h2> Diseño </h2>
-                            <p> Durante la fase de descubrimiento y diseño, nuestro objetivo es ayudar a nuestros clientes a identificar oportunidades de mejora en sus procesos o a conceptualizar y diseñar un producto. Nos enfocamos en tres aspectos fundamentales: funcionalidad, usabilidad y escalabilidad para garantizar que el resultado final sea un producto innovador y de alta calidad. </p>
-                        </div>
+
+                    <div className="title diseño">
+                        <h1> Diseño </h1>
                     </div>
+
+                    <p>
+                        Durante la fase de descubrimiento y diseño, nuestro objetivo es ayudar a nuestros clientes a identificar oportunidades de mejora en sus procesos o a conceptualizar y diseñar un producto. Nos enfocamos en tres aspectos fundamentales: funcionalidad, usabilidad y tecnología para garantizar que el resultado final sea un producto innovador y de alta calidad.
+                    </p>
 
                 </motion.div>
 
                 <motion.div
-                    initial={{ x: '-100%' }}
+                    className="servicios_card"
+                    initial={{ x: '350%' }}
                     whileInView={{ x: '0%' }}
                     transition={{ duration: 0.5 }}
                 >
-                    <div className='servicios_card desarollo'>
-                        <div className='icon'>
-                            <Gear />
-                        </div>
-                        <div className='text'>
-                            <h2> Desarollo </h2>
-                            <p> diseñamos muy bien, lo prometo </p>
-                        </div>
+
+                {/* <div className="servicios_card"> */}
+
+                    <div className="title desarollo">
+                        <h1> Desarollo </h1>
                     </div>
+
+                    <p>
+                        Durante la fase de descubrimiento y diseño, nuestro objetivo es ayudar a nuestros clientes a identificar oportunidades de mejora en sus procesos o a conceptualizar y diseñar un producto. Nos enfocamos en tres aspectos fundamentales: funcionalidad, usabilidad y tecnología para garantizar que el resultado final sea un producto innovador y de alta calidad.
+                    </p>
+
+                {/* </div> */}
+
                 </motion.div>
+
                 <motion.div
-                    initial={{ x: '-100%' }}
+                    className="servicios_card"
+                    initial={{ x: '170%' }}
                     whileInView={{ x: '0%' }}
                     transition={{ duration: 0.5 }}
                 >
-                    <div className='servicios_card soporte'>
-                        <div className='icon'>
-                            <Tools />
-                        </div>
-                        <div className='text'>
-                            <h2> Soporte </h2>
-                            <p> diseñamos muy bien, lo prometo </p>
-                        </div>
+
+                {/* <div className="servicios_card"> */}
+
+                    <div className="title soporte">
+                        <h1> Soporte </h1>
                     </div>
+
+                    <p>
+                        Durante la fase de descubrimiento y diseño, nuestro objetivo es ayudar a nuestros clientes a identificar oportunidades de mejora en sus procesos o a conceptualizar y diseñar un producto. Nos enfocamos en tres aspectos fundamentales: funcionalidad, usabilidad y tecnología para garantizar que el resultado final sea un producto innovador y de alta calidad.
+                    </p>
+
+                {/* </div> */}
+
                 </motion.div>
             </div>
-            <img src={people} alt="" />
+
+            <div className="servicios_icon_container">
+                <Pencil />
+                <Gear />
+                <Tools />
+            </div>
         </section>
     )
 }
