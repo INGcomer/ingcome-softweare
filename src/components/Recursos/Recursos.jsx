ae7e85e29c62f2d2a2adadb72b5547179cfdc6c7
augusto <augustoantonelli88@gmail.com>
2024-12-02 15:50:16 -0300

    agrego recursos de la charla

--- src/components/Recursos/Recursos.jsx
+++ src/components/Recursos/Recursos.jsx
@@ -7,6 +7,9 @@ import './Recursos.css';
 
 // Archivos
 import canvasModeloNegocios from './Archivos/Canvas de Modelo de Negocio.pdf'
+import presentacion1 from './Archivos/Presentacion 1.pdf'
+import giaPlanNegocios from './Archivos/guia_plan_de_negocios.pdf'
+import Osterwalder from './Archivos/Generacion de modelos de negocio-Osterwalder.pdf'
 
 
 export default function Recursos() {
@@ -14,19 +17,33 @@ export default function Recursos() {
         <div className="Recursos">
             <BaseLayout>
                 <h1 className="titulo">Recursos</h1>
+                <h2 className="titulo"> Charla "La brújula para el crecimiento de tu negocio"</h2>
                 <div className="recursos">
                     <div className="recurso">
-                        <h2> Plantilla Canvas Modelo de Negocios </h2>
+                        <h3> Presentacion "La brújula para el crecimiento de tu negocio"</h3>
+                        <a href={presentacion1} target="_blank" rel="noopener noreferrer" download="Presentacion 1.pdf">
+                            <DownloadIcon />
+                        </a>
+                    </div>
+                    <div className="recurso">
+                        <h3> Plantilla Canvas Modelo de Negocios </h3>
                         <a href={canvasModeloNegocios} target="_blank" rel="noopener noreferrer" download="Canvas de Modelo de Negocio.pdf">
                             <DownloadIcon />
                         </a>
                     </div>
                     <div className="recurso">
-                        <h2> Presentacion "La brújula para el crecimiento de tu negocio"</h2>
-                        <a href="">
+                        <h3> Guía Práctica para la Elaboración de un Plan de Negocios </h3>
+                        <a href={giaPlanNegocios} target="_blank" rel="noopener noreferrer" download="guia_plan_de_negocios.pdf">
                             <DownloadIcon />
                         </a>
                     </div>
+                    <div className="recurso">
+                        <h3> Generación de Modelos de Negocio </h3>
+                        <a href={Osterwalder} target="_blank" rel="noopener noreferrer" download="Generacion de modelos de negocio-Osterwalder.pdf">
+                            <DownloadIcon />
+                        </a>
+                    </div>
+
                 </div>
             </BaseLayout>
         </div>
